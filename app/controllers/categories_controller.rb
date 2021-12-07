edfef357a2eba9b705887a0128ec9c632ffebbfa
vgoylo <y_draiv.by@mail.ru>
2021-12-07 15:43:52 +0300

    Add edit, update, destroy for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,37 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(catigory_params)
+      flash[:success] = 'Success'
+      redirect_to categories_path
+    else
+      flash[:error] = 'Error'
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+
+    if @category.destroy
+      flash[:success] = 'Success'
+      redirect_to categories_path
+    else
+      flash[:error] = 'Error'
+    end
+  end
+
+  private
+
+  def catigory_params
+    params.require(:category).permit(:name)
+  end
 end
