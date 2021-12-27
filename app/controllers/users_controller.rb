edcde25a8a28521b551d6880335eaf84139a463e
vgoylo <y_draiv.by@mail.ru>
2021-12-27 18:56:08 +0300

    Make buttons in common style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :avatar)
   end
 
   def sortable_columns
