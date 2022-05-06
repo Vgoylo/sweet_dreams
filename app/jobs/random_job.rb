083f2bbdc1ac80ec473c396dee632c39529fc3cd
Vitali <you@example.com>
2022-05-06 17:25:37 +0300

    Add job for admin in user controller

--- app/jobs/random_job.rb
+++ app/jobs/random_job.rb
@@ -1,8 +1,8 @@
 class RandomJob < ApplicationJob
   queue_as :default
 
-  def perform(user)
-    sleep(20.seconds)
-    puts "Hello #{user} this is a random job"
+  def perform(user_id, start_at, until_at)
+    user = User.find(user_id)
+    p user.dreams.where("created_at >= '#{start_at}' AND created_at <= '#{until_at}'")
   end
 end
