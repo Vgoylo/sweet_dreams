a8555da54f5390e5f6174e85934d0f44b6b275e9
vgoylo <y_draiv.by@mail.ru>
2022-06-14 21:19:26 +0300

    Fix rubocop

--- app/sidekiq/send_message_sidekiq_job.rb
+++ app/sidekiq/send_message_sidekiq_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SendMessageSidekiqJob
   include Sidekiq::Job
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     get 'home/index'
     devise_for :users
 
-    resources :users 
-    
+    resources :users
+
     resources :dreams
     resources :categories
     resources :tags
