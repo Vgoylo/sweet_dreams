7d522c78bcc3a1e771ffd795c4ebec883636ad85
Vitali Hoila <vgoylo91mav@gmail.com>
2023-12-08 10:00:21 +0300

    Optimization to request into db in dream controller for dream private

--- app/query/dreams_list_query.rb
+++ app/query/dreams_list_query.rb
@@ -7,7 +7,7 @@ class DreamsListQuery
   end
 
   def self.dreams_private(current_user, params)
-    Dream.by_user(current_user.id).where(private: params[:private])
+    Dream.select('*').from("(#{self.query(params)}) as dreams")
   end
 
   def self.dreams_by_user_id(params)
@@ -16,5 +16,16 @@ class DreamsListQuery
 
   def self.dreams_by_user_id_false(current_user)
     Dream.by_user(current_user.id).or(Dream.where(private: false))
+  end
+
+  private
+
+  def self.query(params)
+    <<-SQL
+      SELECT * FROM dreams
+      left JOIN users
+      on users.id = dreams.user_id
+      WHERE dreams.private in (#{params[:private]})
+    SQL
   end
 end
